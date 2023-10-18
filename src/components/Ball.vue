88b42c08391fc605f0094a957b82f67fe8705e3d
Carl Hasselblad <carl.hasselblad@wardt.com>
2023-10-18 08:38:18 +0200

    added image

--- src/components/Ball.vue
+++ src/components/Ball.vue
@@ -3,7 +3,16 @@
 </script>
 
 <template>
-  <div class="rounded-full h-10 w-10 bg-white border-2 border-black"></div>
+  <div id="ball" class="rounded-full h-10 w-10 m-1">
+    <!-- <img src="../assets/images/ball_no_bg.png" class="h-20" /> -->
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+#ball {
+  background-color: aqua;
+  background-image: url(../assets/images/ball_round.jpg);
+  background-position: center;
+  background-size: cover;
+}
+</style>
